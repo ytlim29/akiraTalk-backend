f1ce2a03a6700b546324aa3e74433e2d6fd1d510
ytlim29 <coraline.ytlim@gmail.com>
2026-09-12 01:57:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,43 @@ app.post("/api/chat", async (req, res) => {
             });
         }
 
-        // Gemini API will be connected here later
-        res.json({
-            reply: "Backend received your message: " + message
-        });
+      const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-goog-api-key": process.env.GEMINI_API_KEY
+        },
+        body: JSON.stringify({
+            contents: [
+                {
+                    parts: [
+                        {
+                            text: message
+                        }
+                    ]
+                }
+            ]
+        })
+    }
+);
+
+const data = await response.json();
+
+if (!response.ok) {
+    console.error("Gemini API error:", data);
+
+    return res.status(500).json({
+        error: "Gemini API request failed."
+    });
+}
+
+const reply = data.candidates[0].content.parts[0].text;
+
+res.json({
+    reply: reply
+});
 
     } catch (error) {
         console.error(error);
